6df0f36c9f71f2b4f1e1af63003cbbd7a6d889ca
Henrik Nygren <nygrenh@gmail.com>
2017-09-07 17:17:58 +0300

    This is not 2017-ohjelmointi

--- assets/javascripts/quiznator.js
+++ assets/javascripts/quiznator.js
@@ -19,7 +19,7 @@ function onFieldChange(e) {
 
   const quiznatorSpyware = spywares[quizId]
     ? spywares[quizId]
-    : new Spyware(user.username, user.accessToken, '2017-ohjelmointi', exerciseName, 'https://hy.spyware.testmycode.net');
+    : new Spyware(user.username, user.accessToken, 'intro-to-ai-17', exerciseName, 'https://hy.spyware.testmycode.net');
 
   quiznatorSpyware.spyEvent(exerciseName, content, action);
 }
