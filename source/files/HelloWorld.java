dd890d5aa22e842f74e9c1266b63b0f529ec7dec
teemuroos <teemu.roos@cs.helsinki.fi>
2017-10-11 14:35:41 +0300

    fixes

--- source/files/HelloWorld.java
+++ source/files/HelloWorld.java
@@ -4,20 +4,15 @@ import lejos.nxt.*;
 import lejos.robotics.navigation.DifferentialPilot;
 
 /**
- * Example leJOS Project with an ant build file
- *
+ * Write your code in main()
  */
 public class HelloWorld {
-
     static DifferentialPilot pilot = new DifferentialPilot(5.6, 17.5f, Motor.A, Motor.B);
     static UltrasonicSensor ultra = new UltrasonicSensor(SensorPort.S2);
     static LightSensor light = new LightSensor(SensorPort.S1);
 
     public static void main(String[] args) {
-
         System.out.println("I'll be back!");
         Button.waitForAnyPress();
-        
     }
-    
 }
